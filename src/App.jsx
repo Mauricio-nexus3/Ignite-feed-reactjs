2c9fafaeda0393ac4332035fe786424e035994ba
Mauricio-nexus3 <mauriciocostaramos@yahoo.com.br>
2022-07-20 06:20:28 -0300

    Data dinamica com date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,40 @@ import { Post } from "./components/Post/Post"
 import style from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: "https://github.com/Mauricio-nexus3.png",
+      name: "Mauricio Costa",
+      role: 'Developer'
+    },
+    content:[
+      { type: 'paragraph', content:  'Fala galera 👋' },
+      { type: 'paragraph', content:  'Acabei de subir mais um projeto no meu portfólio, é um projeto que fiz no NLW return, evento da rocketseat. O nome do projeto é Hardstore 🚀' },
+      { type: 'link', content:  'John.vendas/hardstore' },
+    ],
+    publishedAt: new Date('2022-07-20 05:50:33')
+  },
+  
+  {
+    id: 2,
+    author:{
+      avatarUrl: "https://github.com/thais.png",
+      name: "Thais Costa",
+      role: 'Ux Designer'
+    },
+
+    content:[
+      { type: 'paragraph', content:  'Oie amores!!' },
+      { type: 'paragraph', content:  'Mais um projeto no meu portfólio, é um projeto que fiz no NLW return, evento da rocketseat. O nome do projeto é Hardstore 🚀' },
+      { type: 'link', content:  'John.vendas/hardstore' },
+    ],
+    publishedAt: new Date('2022-07-19 17:40:33')
+  }
+  
+]
+
 function App() {
  return (
   <div>
@@ -13,11 +47,15 @@ function App() {
         <Sidebar />
         
         <main>
-        <Post
-          author="Diego Fernandes"
-          content='Lorem ipsum....'
-        />
-        
+        {posts.map( post => {
+         return (
+           <Post
+           author={post.author}
+           content={post.content}
+           publishedAt={post.publishedAt}
+           />
+         ) 
+        })}
         </main>
       </div>
   </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,37 +1,41 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { Avatar } from '../Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
-export function Post({author, content}) {
+export function Post({  author, publishedAt, content }) {
+const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
+  locale: ptBR,
+})
+
+const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+  locale: ptBR,
+  addSuffix: true,
+})
+  
   return (
     <article className={styles.post}>
     <header>
       <div className={styles.author}>
-        <Avatar src="https://github.com/Mauricio-nexus3.png" />
+        <Avatar src={author.avatarUrl} />
         <div className={styles.authorInfo}>
-          <strong>Mauricio costa</strong>
-          <p>web developer</p>
+          <strong>{author.name}</strong>
+          <p>{author.role}</p>
         </div>
       </div>
 
-        <time title='11 de maio às 10:10h' dateTime='2022-07-15 10:10:33'>
-          Publicado há 1h
-        </time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
     </header>  
 
     <div className={styles.content}>
-      <p>Fala galera</p>
-      <p>Acabei de subir mais um projeto no meu portfólio, 
-          é um projeto que fiz no NLW return, evento da rocketseat.
-          O nome do projeto é Hardstore.
-      </p>
-  
-      <p><a href="#"> John.vendas/hardstore</a></p>
-      <p>
-          <a href="#"> #novoProjeto </a>
-          <a href="#"> #nlw </a>
-          <a href="#"> #rocketseat </a>
-      </p>
+    { content.map(line => {
+      if (line.type == 'paragraph') {
+        return <p>{line.content}</p>
+      } else if (line.type == 'link') {
+        return <p><a href="#">{line.content}</a></p>
+      }
+    })}
     </div>
 
 
